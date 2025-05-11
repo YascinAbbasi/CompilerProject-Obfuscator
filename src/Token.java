b955fef91f23b8ec4277dea6441c494ab89e808c
YAS <yassinabbasi1697@gmail.com>
2025-05-11 20:59:00 +0330

    pushing the first commit and adding token and token type

---
+++ src/Token.java
@@ -0,0 +1,13 @@
+public class Token {
+    TokenType type;
+    String value;
+
+    Token(TokenType type, String value) {
+        this.type = type;
+        this.value = value;
+    }
+
+    public String toString() {
+        return type + " : " + value;
+    }
+}
